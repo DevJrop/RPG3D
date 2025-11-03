055c8d4f7181b73d2ce5460b522b25b9193e1bb9
JuanJo\Juan Jose <juanjrop002@gmail.com>
2025-11-03 19:45:02 +0100

    Refolding. movement and camera improved. animations added to player

--- IAeNemies/Assets/Scripts/Mover.cs
+++ IAeNemies/Assets/Scripts/Mover.cs
@@ -6,10 +6,11 @@ public class Mover : MonoBehaviour
     [SerializeField] Transform target;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
     private void MoveToCursor()
     {
@@ -21,6 +22,14 @@ public class Mover : MonoBehaviour
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
         
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
     
 }

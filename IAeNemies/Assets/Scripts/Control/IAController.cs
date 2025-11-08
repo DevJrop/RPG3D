cde7a733628a557d4403fcead98251b5d1e24021
JuanJo\Juan Jose <juanjrop002@gmail.com>
2025-11-08 10:46:31 +0100

    Guard and suspicius behavior added to IA

--- IAeNemies/Assets/Scripts/Control/IAController.cs
+++ IAeNemies/Assets/Scripts/Control/IAController.cs
@@ -1,21 +1,28 @@
 using System;
 using Combat;
 using Core;
+using Movement;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class IAController : MonoBehaviour
 {
     [SerializeField] private float chaseDistance;
+    [SerializeField] private float suspiciusTime;
     Fighter fighter;
     GameObject player;
     Health health;
+    Vector3 guardPosition;
+    Mover mover;
+    float timeSinceLastSawPlayer = Mathf.Infinity;
 
     private void Start()
     {
+        mover = GetComponent<Mover>();
         fighter = GetComponent<Fighter>();
         health = GetComponent<Health>();
         player = GameObject.FindGameObjectWithTag("Player");
+        guardPosition = transform.position;
     }
 
     void Update()
@@ -24,18 +31,46 @@ public class IAController : MonoBehaviour
         
         if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
         {
-            fighter.Attack(player);
+            timeSinceLastSawPlayer = 0f;
+            AttackBehaviour();
+        }
+        else if(timeSinceLastSawPlayer < suspiciusTime)
+        {
+            SuspiciusBehavior();
         }
         else
         {
-            fighter.Cancel();
+            GuardBehaviour();
         }
         
+        timeSinceLastSawPlayer += Time.deltaTime;
+    }
+    private void GuardBehaviour()
+    {
+        mover.StartMoveAction(guardPosition);
+    }
+
+    private void SuspiciusBehavior()
+    {
+        GetComponent<ActionScheduler>().CancelCurrentAction();
+    }
+
+    private void AttackBehaviour()
+    {
+        fighter.Attack(player);
     }
 
     private bool InAttackRangeOfPlayer()
     {
-        float distancetoPlayer = Vector3.Distance(player.transform.position,transform.position);
+        float distancetoPlayer = Vector3.Distance(player.transform.position, transform.position);
         return distancetoPlayer < chaseDistance;
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        
     }
 }

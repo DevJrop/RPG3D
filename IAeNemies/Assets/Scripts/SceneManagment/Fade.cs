5b2ddb259582dfa6721d11d2c39c2e723a046243
JuanJo\Juan Jose <juanjrop002@gmail.com>
2025-11-15 13:52:52 +0100

    Fade prefab, script

---
+++ IAeNemies/Assets/Scripts/SceneManagment/Fade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SceneManagment
+{
+    public class Fade : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            StartCoroutine(FadeOutIn(1f));
+        }
+
+        IEnumerator FadeOutIn(float time)
+        {
+            yield return FadeOut(3f);
+            yield return FadeIn(3f);
+        }
+
+        IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+        
+        IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+        
+    }
+}
--- IAeNemies/Assets/Scripts/SceneManagment/Portal.cs
+++ IAeNemies/Assets/Scripts/SceneManagment/Portal.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
@@ -27,7 +28,7 @@ public class Portal : MonoBehaviour
     private void UpdatePlayer(Portal otherPortal)
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        player.transform.position = otherPortal.spawnPoint.position;
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
         player.transform.rotation = otherPortal.spawnPoint.rotation;
     }
     private Portal GetOtherPortal()
